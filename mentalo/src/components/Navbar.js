c6dd911fe88efa127ff947fb659af3f219fbb3fa
Aditya2003tyagi <aditya2003tyagi@gmail.com>
2023-09-15 10:50:02 +0530

    .

--- mentalo/src/components/Navbar.js
+++ mentalo/src/components/Navbar.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div className="bg-blue-500 p-4">
-      <nav>
-        <ul className="flex space-x-4 justify-center">
-          <li className='text-white'>
-            Home
+    <div className="static top-0 left-0 right-0 bg-blue-900 p-4 z-10" id="nav">
+      <nav className="container mx-auto">
+        <ul className=" flex justify-center items-center space-x-4">
+          <li className="text-white hover:text-red-500 cursor-pointer">
+            <Link to="/">Home</Link>
           </li>
-          <li className="text-white">
-            About Us
+          <li className="text-white hover:text-red-500 cursor-pointer">
+            <Link to="/About">About Us</Link>
           </li>
-          <li className="text-white">
-            Join 
+          <li className="text-white hover:text-red-500 cursor-pointer">
+            <Link to="/Join">Join</Link>
           </li>
-          <li className="text-white">
-            Guidelines
+          <li className="text-white hover:text-red-500 cursor-pointer">
+            <Link to="/Guidelines">Guidelines</Link>
+          </li>
+          <li className="ml-auto"> {/* Use ml-auto to push this to the right */}
+            <Link to="/loginform" className="text-white hover:text-red-500 cursor-pointer">
+              Login Now
+            </Link>
           </li>
         </ul>
       </nav>
@@ -24,5 +30,10 @@ function Navbar() {
 }
 
 export default Navbar;
+
+
+
+
+
 
 
